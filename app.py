c76e7fbc991b6ded3d4cba2b6a3765987c304297
spli5nabeel <spli5nabeel@gmail.com>
2023-12-01 11:21:50 +0000

    updates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , url_for, request
+from flask import Flask , render_template , url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -6,6 +6,8 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+
+app.app_context().push()
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,11 +20,19 @@ class Todo(db.Model):
 
 @app.route('/', methods=['POST','GET'])
 def index():
-    if request.method == "POST":
-        return "Hello"
+    if request.method == 'POST':
+        task_content= request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Issue'
     else:
         #return request.method
-        return render_template("index.html")
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html',tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
